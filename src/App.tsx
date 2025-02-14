ce0628a22c090627ed330e55298e521f92c128fa
Sabina Mejlisson <sabinamejlisson@gmail.com>
2025-02-14 09:34:47 +0100

    Added Button function

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import "./App.scss";
 import Counter from "./Counter/Counter";
+import Button from "./Button/Button";
 
 function App() {
 	return (
 		<>
-			<h1>🪴 potted-plant 🪴</h1>
+			<h1>🪴 Potted-plant 🪴</h1>
 			<p>Nu gör vi interaktiva komponenter!</p>
-			<Counter />
-			<Counter />
+			<Counter/>
+			<Counter/>
+			<Button/>
 		</>
 	);
 }
---
+++ src/Button/Button.tsx
@@ -0,0 +1,10 @@
+import "./Button.scss";
+
+const Button = () =>{
+    const handleClick =() =>{
+        console.log("Button clicked!");
+    };
+    return <button onClick={handleClick}> Click Me</button>;
+};
+
+export default Button;
